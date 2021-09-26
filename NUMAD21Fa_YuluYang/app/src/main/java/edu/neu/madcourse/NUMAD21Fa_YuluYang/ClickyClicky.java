a64823464c00aeeea8741e8db8faef1128fdc3ae
yangyulu1114 <yangyulu1114@163.com>
2021-09-25 22:16:48 -0700

    add clicky_click xml

---
+++ NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/ClickyClicky.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.NUMAD21Fa_YuluYang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClickyClicky extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clicky_clicky);
+    }
+}
