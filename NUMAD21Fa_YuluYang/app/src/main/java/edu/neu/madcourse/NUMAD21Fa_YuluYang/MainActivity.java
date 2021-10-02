5bfa24f9594ed357562242623688e2f148feae23
yangyulu1114 <yangyulu1114@163.com>
2021-10-02 16:20:52 -0700

    add linkActivity

--- NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/MainActivity.java
+++ NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,15 +16,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClickAbout(View view) {
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.aboutBtn :
+                onClickAbout();
+                break;
+            case R.id.clicky_clicky:
+                Intent intent = new Intent(MainActivity.this, ClickyClickyActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.link_collector:
+                Intent intent2 = new Intent(MainActivity.this, LinkActivity.class);
+                startActivity(intent2);
+                break;
+        }
+    }
+    private void onClickAbout() {
         // fix for hiding package name on xiaomi phone
         Toast toast = Toast.makeText(this, null, Toast.LENGTH_SHORT);
         toast.setText(getString(R.string.about_toast));
         toast.show();
-    }
-
-    public void onClickyClicky(View view) {
-        Intent intent = new Intent(MainActivity.this, ClickyClickyActivity.class);
-        startActivity(intent);
     }
 }
