85e548f4df9678d26fee6f8dc3eee54e8fe31817
yangyulu1114 <yangyulu1114@163.com>
2021-10-02 19:07:23 -0700

    add floatActionButton and SnackBar

--- NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/LinkActivity.java
+++ NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/LinkActivity.java
@@ -1,14 +1,27 @@
 package edu.neu.madcourse.NUMAD21Fa_YuluYang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LinkActivity extends AppCompatActivity {
+
+    private ConstraintLayout mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link);
+        mLayout = findViewById(R.id.layout);
+    }
+
+    public void onClick(View view) {
+        Snackbar.make(mLayout, "SnakBar", Snackbar.LENGTH_SHORT).show();
     }
 }
