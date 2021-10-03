649f4eb0f8501e0a3e44615f752878b2f286fdba
yangyulu1114 <yangyulu1114@163.com>
2021-10-02 21:07:19 -0700

    modify dialog

--- NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/LinkActivity.java
+++ NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/LinkActivity.java
@@ -8,31 +8,31 @@ import androidx.constraintlayout.widget.ConstraintSet;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class LinkActivity extends AppCompatActivity {
 
-    private ConstraintLayout mLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link);
-        mLayout = findViewById(R.id.layout);
     }
 
     public void onClick(View view) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("please enter name and url");
-        final EditText edit = new EditText(this);
-        builder.setView(edit);
+        builder.setTitle("Please enter name and url");
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog, null);
+        builder.setView(dialogView);
         builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Snackbar.make(mLayout, "Add success", Snackbar.LENGTH_SHORT).show();
+                View root = getWindow().getDecorView().findViewById(android.R.id.content);
+                Snackbar.make(root, "Add success", Snackbar.LENGTH_SHORT).show();
             }
         });
         AlertDialog dialog = builder.create();
