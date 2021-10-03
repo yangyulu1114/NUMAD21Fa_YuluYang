3a0ad4abb3505d0c8ca207c7f87165dfc6500786
yangyulu1114 <yangyulu1114@163.com>
2021-10-02 20:02:21 -0700

    add input dialog

--- NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/LinkActivity.java
+++ NUMAD21Fa_YuluYang/app/src/main/java/edu/neu/madcourse/NUMAD21Fa_YuluYang/LinkActivity.java
@@ -1,12 +1,15 @@
 package edu.neu.madcourse.NUMAD21Fa_YuluYang;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.widget.EditText;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -22,6 +25,18 @@ public class LinkActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
-        Snackbar.make(mLayout, "SnakBar", Snackbar.LENGTH_SHORT).show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("please enter name and url");
+        final EditText edit = new EditText(this);
+        builder.setView(edit);
+        builder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Snackbar.make(mLayout, "Add success", Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.show();
     }
 }
